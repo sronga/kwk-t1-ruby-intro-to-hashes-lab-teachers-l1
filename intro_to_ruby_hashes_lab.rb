939b16add606b2366d80dc07f79c7a9f0ef1fb26
sronga <sr899@georgetown.edu>
2018-07-19 00:47:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,9 @@ def monopoly_with_third_tier
 								{
 									pieces: 4,
 									names: {},
-									rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50}
+									rent_in_dollars: {one_piece_owned: 25,
+										two_pieces_owned: 50,
+										three_pieces_owned: 100}
 								}
 							}
 end
